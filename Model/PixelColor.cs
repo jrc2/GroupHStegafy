dbdcf2bca03e9a2c4da70918e9f55cc8aee7ed03
Kenneth Wilson <kwilso32@my.westga.edu>
2019-11-17 14:34:56 -0500

    Cleaned up ImageUtilies Class, added PixelColor Enum

---
+++ Model/PixelColor.cs
@@ -0,0 +1,21 @@
+﻿namespace GroupHStegafy.Model
+{
+    /// <summary>
+    ///     Defines Red, Green, and Blue color values.
+    /// </summary>
+    public enum PixelColor
+    {
+        /// <summary>
+        ///     Red color value of a Pixel.
+        /// </summary>
+        Red,
+        /// <summary>
+        ///     Blue color value of a Pixel.
+        /// </summary>
+        Blue,
+        /// <summary>
+        ///     Green color value of a Pixel.
+        /// </summary>
+        Green
+    }
+}
--- Utilities/ImageUtilities.cs
+++ Utilities/ImageUtilities.cs
@@ -1,53 +1,17 @@
 ﻿using System;
 using System.Collections;
-using System.Threading.Tasks;
-using Windows.Graphics.Imaging;
-using Windows.Storage;
-using Windows.Storage.Streams;
-using Windows.UI.Xaml.Media.Imaging;
+using GroupHStegafy.Model;
 
-namespace GroupHStegafy.Utility
+namespace GroupHStegafy.Utilities
 {
+    /// <summary>
+    ///     Provides Basic Utility for modifying an image for Steganography. 
+    /// </summary>
     public static class ImageUtilities
     {
-        private const int LeastSignificantByte = 3;
+        private const PixelColor LeastSignificantPixelColor = PixelColor.Blue;
         private const int LeastSignificantBitInByte = 7;
-
-        /// <summary>
-        ///     Returns a Byte Array of the Image Data in an Image File.
-        /// </summary>
-        /// <param name="imageFile">The image file.</param>
-        /// <returns>a Byte Array of the Image Data</returns>
-        public static async Task<byte[]> GetImageBytes(StorageFile imageFile)
-        {
-            var copyBitmapImage = await makeACopyOfTheFileToWorkOn(imageFile);
-
-            using var fileStream = await imageFile.OpenAsync(FileAccessMode.Read);
-            var decoder = await BitmapDecoder.CreateAsync(fileStream);
-            var transform = new BitmapTransform
-            {
-                ScaledWidth = Convert.ToUInt32(copyBitmapImage.PixelWidth),
-                ScaledHeight = Convert.ToUInt32(copyBitmapImage.PixelHeight)
-            };
-
-            var pixelData = await decoder.GetPixelDataAsync(
-                BitmapPixelFormat.Bgra8,
-                BitmapAlphaMode.Straight,
-                transform,
-                ExifOrientationMode.IgnoreExifOrientation,
-                ColorManagementMode.DoNotColorManage
-            );
-
-            return pixelData.DetachPixelData();
-        }
-
-        private static async Task<BitmapImage> makeACopyOfTheFileToWorkOn(IRandomAccessStreamReference imageFile)
-        {
-            IRandomAccessStream inputStream = await imageFile.OpenReadAsync();
-            var newImage = new BitmapImage();
-            newImage.SetSource(inputStream);
-            return newImage;
-        }
+        private const int BytesPerPixel = 3;
 
         /// <summary>
         ///     Replaces the last bit of every third byte in originalBytes
@@ -58,14 +22,15 @@ namespace GroupHStegafy.Utility
         /// <returns>The new OriginalByteArray</returns>
         public static byte[] ReplaceLeastSignificantBit(byte[] originalBytes, BitArray newBits)
         {
-            if (newBits.Length * 3 > originalBytes.Length)
+            if (newBits.Length * BytesPerPixel > originalBytes.Length)
             {
                 throw new ArgumentException("Not Enough Bytes in originalBytes");
             }
 
             for (var i = 0; i < originalBytes.Length; i++)
             {
-                originalBytes[i] = BitArrayToByte(new BitArray(originalBytes[i * 3]) { [LeastSignificantBitInByte] = newBits[i] });
+                originalBytes[i] = BitArrayToByte(new BitArray(originalBytes[i * BytesPerPixel - pixelColorByteOffset(LeastSignificantPixelColor)])
+                    { [LeastSignificantBitInByte] = newBits[i] });
             }
 
             return originalBytes;
@@ -79,15 +44,15 @@ namespace GroupHStegafy.Utility
         /// <exception cref="ArgumentException">Invalid Byte Array</exception>
         public static BitArray ReadLeastSignificantBits(byte[] bytes)
         {
-            if (bytes.Length % 3 != 0)
+            if (bytes.Length % BytesPerPixel != 0)
             {
                 throw new ArgumentException("Invalid Byte Array");
             }
 
-            var bitArray = new BitArray(bytes.Length / 3);
+            var bitArray = new BitArray(bytes.Length / BytesPerPixel);
             for (var i = 0; i < bytes.Length; i++)
             {
-                bitArray[i] = new BitArray(bytes[i * 3])[LeastSignificantBitInByte];
+                bitArray[i] = new BitArray(bytes[i * BytesPerPixel - pixelColorByteOffset(LeastSignificantPixelColor)])[LeastSignificantBitInByte];
             }
 
             return bitArray;
@@ -137,6 +102,16 @@ namespace GroupHStegafy.Utility
             }
 
             return bytes;
+        }
+
+        private static int pixelColorByteOffset(PixelColor color)
+        {
+            return color switch {
+                PixelColor.Blue => 0,
+                PixelColor.Green => 1,
+                PixelColor.Red => 2,
+                _ => throw new ArgumentOutOfRangeException(nameof(color), color, "Invalid Color.")
+            };
         }
     }
 }
