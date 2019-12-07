7b4d53a7447109763c6905c05a31a0c82d7e7d66
John Chittam <john@jchittam.com>
2019-12-07 17:06:58 -0500

    added TextUtilities static class and text encryption method

---
+++ Utilities/TextUtilities.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace GroupHStegafy.Utilities
+{
+    public static class TextUtilities
+    {
+        public static string ExpandKey(int length, string key)
+        {
+            var expandedKey = "";
+
+            for (int strPosition = 0, keyPosition = 0; strPosition < length; strPosition++, keyPosition++)
+            {
+                if (keyPosition == key.Length)
+                {
+                    keyPosition = 0;
+                }
+
+                expandedKey += key.ToUpper()[keyPosition];
+            }
+
+            return expandedKey;
+        }
+
+        public static string EncryptText(string text, string expandedKey)
+        {
+            var encryptedText = "";
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                var isLowercase = char.IsLower(text[i]);
+                var numericEquivalent = (text.ToUpper()[i] + expandedKey[i]) % 26;
+                var letterToAdd = (char) (numericEquivalent += 'A');
+                if (isLowercase)
+                {
+                    encryptedText += char.ToLower(letterToAdd);
+                }
+                else
+                {
+                    encryptedText += letterToAdd;
+                }
+            }
+
+            return encryptedText;
+        }
+    }
+}
